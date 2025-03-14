f3f516055c317d9f018743c78b220b7d7f8c3f85
Anveshreddyvisarapu <73732013+Anveshreddyvisarapu@users.noreply.github.com>
2025-03-14 10:13:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
@@ -31,4 +32,5 @@ def logout():
     return redirect(url_for('user_agreement'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
